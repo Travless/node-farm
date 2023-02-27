6ecfcded78040302c38e8af74c7144b5a6c295b9
Travless <90566281+Travless@users.noreply.github.com>
2023-02-27 13:41:58 -0500

    Map product data parsed from JSON file to product card templates on the overview page

--- index.js
+++ index.js
@@ -38,6 +38,23 @@ const url = require('url');
 // console.log('Will read file!')
 
 ///////////////////SERVER////////////////////////////
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic) {
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+        return output;
+    }
+    return output;
+}
+
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
@@ -51,10 +68,11 @@ const server = http.createServer((req, res) => {
 
     // Overview page
     if (pathName === '/' || pathName === '/overview') {
-        res.writeHead(200, {
-            'Content-type': 'text/html'
-        });
-        res.end(tempOverview);
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+        res.end(output);
 
         // Product page
     } else if (pathName === '/product') {
